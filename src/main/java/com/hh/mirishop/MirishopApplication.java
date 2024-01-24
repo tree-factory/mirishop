807618507b22b33d803c7daa6488ec5d2c585899
noyes5 <noyes5@naver.com>
2024-01-24 18:00:15 +0900

    first project commit

---
+++ src/main/java/com/hh/mirishop/MirishopApplication.java
@@ -0,0 +1,13 @@
+package com.hh.mirishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirishopApplication.class, args);
+	}
+
+}
