2d2caacb18c030e403024e41981f529ac725f60d
noyes5 <noyes5@naver.com>
2024-02-04 21:33:24 +0900

    refactor: 이메일 에러 관련 리팩토링

--- src/main/java/com/hh/mirishop/common/exception/ExceptionManager.java
+++ src/main/java/com/hh/mirishop/common/exception/ExceptionManager.java
@@ -20,6 +20,12 @@ public class ExceptionManager extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(errorCode);
     }
 
+    @ExceptionHandler(EmailException.class)
+    protected ResponseEntity<?> handleEmailException(JwtTokenException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return handleExceptionInternal(errorCode);
+    }
+
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus())
                 .body(makeErrorResponse(errorCode));
