d71f22bdc6f6421e650f1925e03fb2fc697c7c97
noyes5 <noyes5@naver.com>
2024-02-17 13:38:12 +0900

    feat: user_service 개별 repository로 분리

--- user_service/src/main/java/com/hh/mirishop/user/auth/service/LogoutService.java
+++
@@ -1,6 +0,0 @@
-package com.hh.mirishop.user.auth.service;
-
-public interface LogoutService {
-
-    void logout(Long memberNumber);
-}
--- user_service/src/main/java/com/hh/mirishop/user/client/UserServiceClient.java
+++
@@ -1,7 +0,0 @@
-package com.hh.mirishop.user.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "user-service", url = "${feign.user-service.url")
-public interface UserServiceClient {
-}
